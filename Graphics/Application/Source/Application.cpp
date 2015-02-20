6344bc8d70555ebd3724e5343c87570411b7f8bf
Setsuna <sweeseng123@gmail.com>
2015-02-21 02:37:10 +0800

    did some random stuff plus TGA files for SP2

--- Graphics/Application/Source/Application.cpp
+++ Graphics/Application/Source/Application.cpp
@@ -113,7 +113,7 @@ void Application::Init()
 void Application::Run()
 {
 	//Main Loop
-	Scene *scene = new Assignment3();
+	Scene *scene = new SceneLight();
 	scene->Init();
 
 	m_timer.startTimer();    // Start timer to calculate how long it takes to render this frame

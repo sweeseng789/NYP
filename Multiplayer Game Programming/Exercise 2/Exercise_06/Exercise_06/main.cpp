715c723fbcbcd3867654171a1dc4008f94684afb
Setsuna <sweeseng123@gmail.com>
2015-10-27 22:56:31 +0800

    Did multiplayer Exercise 2

---
+++ Multiplayer Game Programming/Exercise 2/Exercise_06/Exercise_06/main.cpp
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include <string.h>
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define BUFSIZE 100
+#define PORT_NUMBER 7890
+int main(int argc, char **argv)
+{
+	int Port = PORT_NUMBER;
+	WSADATA WsaData;
+	SOCKET ServerSocket;
+	SOCKADDR_IN ServerAddr;
+	unsigned int Index;
+	int ClientLen = sizeof(SOCKADDR_IN);
+	SOCKET ClientSocket;
+	SOCKADDR_IN ClientAddr;
+	fd_set ReadFds, TempFds;
+	TIMEVAL Timeout; // struct timeval timeout;
+	char Message[BUFSIZE];
+	int Return;
+	if (2 == argc)
+	{
+		Port = atoi(argv[1]);
+	}
+	printf("Using port number : [%d]\n", Port);
+	if (WSAStartup(MAKEWORD(2, 2), &WsaData) != 0)
+	{
+		printf("WSAStartup() error!\n");
+		return 1;
+	}
+	ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (INVALID_SOCKET == ServerSocket)
+	{
+		printf("socket() error\n");
+		return 1;
+	}
+
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_port = htons(Port);
+	ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (SOCKET_ERROR == bind(ServerSocket, (SOCKADDR *)&ServerAddr, sizeof(ServerAddr)
+		))
+	{
+		printf("bind() error\n");
+		return 1;
+	}
+	if (SOCKET_ERROR == listen(ServerSocket, 5))
+	{
+		printf("listen() error\n");
+		return 1;
+	}
+	FD_ZERO(&ReadFds);
+	FD_SET(ServerSocket, &ReadFds);
+	while (1)
+	{
+		TempFds = ReadFds;
+		Timeout.tv_sec = 5;
+		Timeout.tv_usec = 0;
+		if (SOCKET_ERROR == (Return = select(0, &TempFds, 0, 0, &Timeout)))
+		{ // Select() function returned error.
+			printf("select() error\n");
+			return 1;
+		}
+
+		if (0 == Return)
+		{ // Select() function returned by timeout.
+			printf("Select returned timeout.\n");
+		}
+		else if (0 > Return)
+		{
+			printf("Select returned error!\n");
+		}
+		else
+		{
+			for (Index = 0; Index < TempFds.fd_count; Index++)
+			{
+				if (TempFds.fd_array[Index] == ServerSocket)
+				{ // New connection requested by new client.
+					ClientSocket = accept(ServerSocket, (SOCKADDR *)&ClientAddr, &ClientLen);
+					FD_SET(ClientSocket, &ReadFds);
+
+					//Return Msg
+					std::string msgToReturn = "Welcome client with Session ID: ";
+					msgToReturn += std::to_string(ClientSocket);
+					const char* ReturnMsg = msgToReturn.c_str();
+					send(ClientSocket, ReturnMsg, strlen(ReturnMsg), 0);
+
+					std::string msgToReturn2 = "New client with Session ID: ";
+					msgToReturn2 += std::to_string(ClientSocket);
+					const char* ReturnMsg2 = msgToReturn2.c_str();
+
+					for (unsigned a = 0; a < ReadFds.fd_count; ++a)
+					{
+						if (ReadFds.fd_array[a] != ClientSocket)
+							send(ReadFds.fd_array[a], ReturnMsg2, strlen(ReturnMsg2), 0);
+					}
+
+					printf("New Client Accepted : Socket Handle [%d]\n", ClientSocket);
+				}
+				else
+				{ // Something to read from socket.
+					Return = recv(TempFds.fd_array[Index], Message, BUFSIZE, 0);
+					if (0 == Return)
+					{ // Connection closed message has arrived.
+						closesocket(TempFds.fd_array[Index]);
+						printf("Connection closed :Socket Handle [%d]\n",
+							TempFds.fd_array[Index]);
+						FD_CLR(TempFds.fd_array[Index], &ReadFds);
+					}
+					else if (0 > Return)
+					{ // recv() function returned error.
+						closesocket(TempFds.fd_array[Index]);
+						printf("Exceptional error :Socket Handle [%d]\n",
+							TempFds.fd_array[Index]);
+						FD_CLR(TempFds.fd_array[Index], &ReadFds);
+					}
+					else
+					{ // Message recevied.
+						std::string recvMsg = "";
+						std::string clientSessionNum = "";
+						std::string msgToSendOut = "";
+						int lastKnownNum = 0;
+
+						clientSessionNum.resize(strlen(Message));
+
+						//Getting Session ID
+						for (unsigned a = 0; a < Return; ++a)
+						{
+							recvMsg += Message[a];
+						}
+						std::cout << recvMsg << std::endl;
+
+						if (recvMsg[0] == '/')
+						{
+							for (unsigned a = 1; a < recvMsg.length(); a++)
+							{
+								if (recvMsg[a] != '\0' && isdigit(recvMsg[a]))
+								{
+									clientSessionNum[a] = recvMsg[a];
+								}
+								else
+								{
+									lastKnownNum = a;
+									break;
+								}
+							}
+						}
+
+						clientSessionNum.erase(0, 1);
+						clientSessionNum.resize(lastKnownNum - 1);
+
+						//Getting message
+						for (unsigned a = lastKnownNum + 1; a < recvMsg.length(); a++)
+						{
+							msgToSendOut += recvMsg[a];
+						}
+
+						std::string msgToSendOver = "Client Session " + std::to_string(TempFds.fd_array[Index]) + " sent:" + msgToSendOut;
+
+						//Send Msg
+						for (unsigned a = 0; a < ReadFds.fd_count; ++a)
+						{
+							if (std::stoi(clientSessionNum) == static_cast<int>(ReadFds.fd_array[a]))
+							{
+								Return = send(ReadFds.fd_array[a], msgToSendOver.c_str(), strlen(msgToSendOver.c_str()), 0);
+
+								std::string msgtoReturn = "";
+								if (Return != SOCKET_ERROR)
+								{
+									msgtoReturn = "Send Complete";
+								}
+								else
+								{
+									msgtoReturn = "Send Error!";
+								}
+								send(TempFds.fd_array[Index], msgtoReturn.c_str(), strlen(msgtoReturn.c_str()), 0);
+
+								break;
+							}
+						}
+
+						
+						//send(TempFds.fd_array[Index], Message, Return, 0);
+					}
+				}
+			}
+		}
+	}
+	WSACleanup();
+	return 0;
+}
